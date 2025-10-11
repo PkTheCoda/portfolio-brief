8583312dd07f3b0b694090459b1032dd6a562668
PkTheCoda <pranavkonjeti@gmail.com>
2025-10-10 21:42:32 -0500

    changed highlight button structure

--- src/Components/HeroBrief.jsx
+++ src/Components/HeroBrief.jsx
@@ -3,7 +3,7 @@ import { BsGithub } from "react-icons/bs";
 
 const HeroBrief = () => {
   return (
-    <div className="bg-[#f8e1bf] h-max px-6 sm:px-20 pt-20 text-[#694023] font-outfit flex items-center justify-center">
+    <div className="bg-[#f4e2c6] h-max px-6 sm:px-20 pt-10 text-[#694023] font-outfit flex items-center justify-center">
       {/* <div className="absolute py-2 top-0 bg-[#794a29] w-full text-white text-center px-2">
         This is a <i>brief</i> version of my portfolio, find more about my work
         on{" "}
@@ -19,11 +19,12 @@ const HeroBrief = () => {
 
       <div className="flex flex-col gap-y-6 max-w-[40rem] text-lg">
         <div className="flex flex-col gap-y-1">
-          <h1 className="four-title font-medium">Hey 👋, I'm Pranav</h1>
+          <h1 className="four-title font-medium">Hey! I'm Pranav</h1>
           <p className="">
-            I'm a web developer and designer from Virginia. I like designing 
-            websites with beautiful UI and building impactful EdTech for students.
-            I'm currently starting my freshman year at UIUC studying <span className="font-semibold">Computer Science.</span>
+            I'm a web developer and designer from Virginia. I like designing
+            websites with beautiful UI and building impactful EdTech for
+            students. I'm currently a freshman studying @ UIUC studying{" "}
+            <span className="font-semibold">Computer Science.</span>
           </p>
         </div>
 
@@ -31,7 +32,6 @@ const HeroBrief = () => {
           <h1 className="four-title font-medium">What I'm doing/did:</h1>
 
           <div className="space-y-4">
-
             <div className="space-y-2">
               <h4>
                 Worked @ {" "}
@@ -41,41 +41,23 @@ const HeroBrief = () => {
                   target="_blank"
                 >
                   Ultra
-                </a>
-                {' '}as a Software Engineering Intern
+                </a>{" "}
+                as a Software Engineering Intern
               </h4>
               <ul className="list-disc pl-10">
                 <li>
-                  Ultra is a Y-Combinator (YC W24) backed startup serving as the ultimate career coach for high-achieving high school students.
+                  Ultra is a Y-Combinator (YC W24) backed startup serving as the
+                  ultimate career coach for high-achieving high school students.
                 </li>
                 <li>
-                  I specifically helped design from the ground up landing pages for features and programs (homepage, fellowships, college evaluation)
-                  and helped build/refactor existing features (roadmap, opportunities, etc.)
+                  I specifically helped design from the ground up landing pages
+                  for features and programs (homepage, fellowships, college
+                  evaluation) and helped build/refactor existing features
+                  (roadmap, opportunities, etc.)
                 </li>
               </ul>
             </div>
 
-            <div className="space-y-2">
-              <h4>
-                Building{" "}
-                <a
-                  href="https://talem.org"
-                  className="highlight"
-                  target="_blank"
-                >
-                  talem
-                </a>
-                , a platform connecting high schoolers w/ opportunities
-              </h4>
-              <ul className="list-disc pl-10">
-                <li>
-                  1.2M page views, 200k unique users, 400~ daily users to date.
-                </li>
-                <li>
-                  Raised grants from organizations like Emergent Ventures (31st cohort) to continue platform's mission.
-                </li>
-              </ul>
-            </div>
             <div className="space-y-2">
               <h4>
                 Running{" "}
@@ -93,9 +75,7 @@ const HeroBrief = () => {
                   I've worked with over 25 businesses/nonprofits around the
                   world.
                 </li>
-                <li>
-                  Generated low 5-figure revenue within a year
-                </li>
+                <li>Generated low 5-figure revenue within a year</li>
                 <li>
                   Here is some of my agency's{" "}
                   <a
@@ -112,56 +92,79 @@ const HeroBrief = () => {
                 </li>
               </ul>
             </div>
+
+            <div className="space-y-2">
+              <h4>
+                Built{" "}
+                <a
+                  href="https://talem.org"
+                  className="highlight"
+                  target="_blank"
+                >
+                  talem
+                </a>
+                , a platform connecting high schoolers w/ opportunities
+              </h4>
+              <ul className="list-disc pl-10">
+                <li>
+                  1.2M page views, 200k unique users, 400~ daily users to date.
+                </li>
+                <li>
+                  Raised grants from organizations like Emergent Ventures (31st
+                  cohort) to continue platform's mission.
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
 
         <div className="flex flex-col gap-y-1">
-          <h1 className="four-title font-medium">Miscellaneous:</h1>
+          <h1 className="four-title font-medium">Misc:</h1>
 
           <div className="space-y-4">
             <div className="space-y-4">
-            <div className="space-y-2">
-              <h4>I love Indie Hacking & Building useful (?) stuff online</h4>
-              <ul className="list-disc pl-10 space-y-2">
-                <li>
-                  I built{" "}
-                  <a
-                    href="https://desourcedesign.vercel.app/"
-                    className="highlight"
-                    target="_blank"
-                  >
-                    desource
-                  </a>
-                  , an open directory of free web-development resources
-                </li>
-                <li>
-                  Built a{" "}
-                  <a
-                    href="https://ecoeats.us"
-                    target="_blank"
-                    className="highlight"
-                  >
-                    sustainable food app
-                  </a>{" "}
-                  with some friends, an{" "}
-                  <a
-                    href="https://www.youtube.com/watch?v=ZjpzYi6aBa8"
-                    target="_blank"
-                    className="highlight"
-                  >
-                    AI-language learning app
-                  </a>
-                  , and a website that simplifies{" "}
-                  <a
-                    href="https://www.linkedin.com/posts/pranav-konjeti_easyaps-ai-can-now-grade-your-frq-response-activity-7220802778858696704-3VPA?utm_source=share&utm_medium=member_desktop"
-                    target="_blank"
-                    className="font-semibold underline bg-[#ebcfa8] px-1 py-px rounded-md"
-                  >
-                    AP exam prep
-                  </a>
-                </li>
-              </ul>
-            </div>
+              <div className="space-y-2">
+                <h4>I love Indie Hacking & building fun, user-facing products online</h4>
+                <ul className="list-disc pl-10 space-y-2">
+                  <li>
+                    Built{" "}
+                    <a
+                      href="https://desourcedesign.vercel.app/"
+                      className="highlight"
+                      target="_blank"
+                    >
+                      desource
+                    </a>
+                    , an open directory of free web-development resources
+                  </li>
+                  <li>
+                    Built a{" "}
+                    <a
+                      href="https://ecoeats.us"
+                      target="_blank"
+                      className="highlight"
+                    >
+                      sustainable food app
+                    </a>{" "}
+                    with some friends, an{" "}
+                    <a
+                      href="https://www.youtube.com/watch?v=ZjpzYi6aBa8"
+                      target="_blank"
+                      className="highlight"
+                    >
+                      AI-language learning app
+                    </a>
+                    , and a website that simplifies{" "}
+                    <a
+                      href="https://www.linkedin.com/posts/pranav-konjeti_easyaps-ai-can-now-grade-your-frq-response-activity-7220802778858696704-3VPA?utm_source=share&utm_medium=member_desktop"
+                      target="_blank"
+                      className="highlight"
+                    >
+                      AP exam prep
+                    </a>
+                  </li>
+                </ul>
+              </div>
               <ul className="list-disc pl-2 space-y-4">
                 <li>
                   I was selected as a 2025 Taco Bell Live Más Scholar out of
@@ -180,9 +183,9 @@ const HeroBrief = () => {
                     My Portfolio
                   </a>
                 </li> */}
-                <li>
+                {/* <li>
                   I love staying active: I played varsity volleyball for my school team, go to the gym, and love the occasional tennis/pickleball match
-                </li>
+                </li> */}
               </ul>
             </div>
           </div>
